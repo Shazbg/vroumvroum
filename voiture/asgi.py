8db682e3874a3514dd047bcfe4b32d7e690a6be6
Shazir Sheik <shazir.sheik@telecom-sudparis.eu>
2024-09-12 15:03:50 +0200

    changes

--- voiture/asgi.py
+++ voiture/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for voiture_v2 project.
+ASGI config for voiture project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voiture_v2.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voiture.settings")
 
 application = get_asgi_application()
