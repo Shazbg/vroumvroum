421d3c8e9f0c15fdd6b73af9170a16c381ef575c
Shazir Sheik <shazir.sheik@telecom-sudparis.eu>
2024-09-13 09:51:32 +0200

    remove date pret/rendu pour passer en reservaton

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
-from PIL import Image
+
 
 class Garage(models.Model):
     nom = models.CharField(max_length=30)
@@ -23,28 +23,9 @@ class Voiture(models.Model):
 
     
 
-
-
 class Cle(models.Model):
-    etat_pret = models.BooleanField()
-    date_pret = models.DateField(blank=True, null=True)
-    date_rendu = models.DateField(blank=True, null=True)
     voiture = models.ForeignKey(Voiture, on_delete=models.CASCADE)
 
-    def clean(self):
-        if self.etat_pret:
-            if not self.date_pret or not self.date_rendu:
-                raise ValidationError("Les champs 'date_pret' et 'date_rendu' sont obligatoires si 'etat_pret' est vrai.")
-            if self.date_pret > self.date_rendu:
-                raise ValidationError("'date_pret' doit être antérieure ou égale à 'date_rendu'.")
-        else:
-            if self.date_pret or self.date_rendu:
-                raise ValidationError("Les champs 'date_pret' et 'date_rendu' doivent être vides si 'etat_pret' est faux.")
-
-    def save(self, *args, **kwargs):
-        # merci chatGPT
-        self.clean()
-        super().save(*args, **kwargs)
     def __str__(self):
         return f"clé de {self.voiture}"
 
